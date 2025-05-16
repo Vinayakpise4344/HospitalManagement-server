a4a34ae93a383c7024cc05580e59748a98b8639f
Vinayak <pisevinayak56@gmail.com>
2025-05-16 21:06:21 +0530

    Some Changes

--- src/main/java/HospitalManagementWeb/Config/CorsConfig.java
+++ src/main/java/HospitalManagementWeb/Config/CorsConfig.java
@@ -19,7 +19,7 @@ public class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
 
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173", "https://hospitalmanagmentsystem.vercel.app") // Specify the origin explicitly
+                        .allowedOriginPatterns("http://localhost:5173", "https://hospitalmanagmentsystem.vercel.app") // Specify the origin explicitly
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*") // Allow any headers
                         .allowCredentials(true); // Allow credentials (cookies, headers, etc.
